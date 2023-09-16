7075c329331c043d3bb5371a786e61de6a3e7cd4
MADESH122 <madeshwaran9080@gmail.com>
2023-09-16 14:55:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function foo(){
+    var first =document.getElementById("Firstname").value;
+    var Middle =document.getElementById("Middlename").value;
+    var Last =document.getElementById("Lastname").value;
+    var Email =document.getElementById("Email").value;
+    var Pin =document.getElementById("Pincode").value;
+    console.log(first)
+    console.log(Middle)
+    console.log(Last)
+    console.log(Email)
+    console.log(Pin)
+ }
